cac838f71c2b5e2057b322c90f8b0b965a8078df
Deathik <smertelny@gmail.com>
2018-02-07 09:24:08 +0200

    Homework #1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repository is made for BrainBasket WowTeachers course
 
 I will be posting my homeworks here
+
+You can see my work <a href="https://smertelny.github.io/brainbasket_course/">here</a>
---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+document.getElementById("buy_prodcut").addEventListener('click', function () {
+    alert("Congradulations! You bought this product!\n" + 
+    "P.S. Not really, this is just dummy script");
+});
