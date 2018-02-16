c73c95d9c26dcc11bec15efe2854cdb3921f7ba7
Deathik <smertelny@gmail.com>
2018-02-16 10:21:14 +0200

    Homework #3 (Easy, flexbox)

--- static/js/form-script.js
+++ static/js/form-script.js
@@ -4,5 +4,11 @@ var btn = document.getElementById("form-submit");
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
-    alert("Not implemented yet");
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email && password) {
+        alert("Not implemented yet");
+    } else {
+        alert("Insert valid data");
+    }
 });
